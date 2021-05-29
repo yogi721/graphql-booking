b066941000d1bea04aef91c3892f9933fb8ccc8b
Mehdi <semlali.mehdi.1980@gmail.com>
2021-05-29 15:13:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.listen(3333)
