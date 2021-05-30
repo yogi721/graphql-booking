7e214ed6a9389a721851926bdedb75ddcbac83ee
Mehdi <semlali.mehdi.1980@gmail.com>
2021-05-30 06:51:21 +0200

    adding schema resolver types and data

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
 const express = require('express');
-// deprecated: const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express();
 
-// deprecated: app.use(bodyParser.json())
-// deprecated: app.use(bodyParser.urlencoded({extended: true}));
+const events = [];
 
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-    res.send("Hello world!")
-})
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery{
+            events: [Event!]!
+        }
+
+        type RootMutation{
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+        rootValue: {
+            events: () => {
+                return events;
+            },
+            createEvent: (args) => {
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: args.eventInput.date
+                };
+                events.push(event);
+            }
+        },
+        graphiql: true
+    }))
 
 app.listen(3333)
